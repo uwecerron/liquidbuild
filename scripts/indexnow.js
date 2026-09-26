42a7fce0dbd538297e2d3f0be454685e4cb0229c
Uwe Cerron <uwecerron@gmail.com>
2026-09-26 15:19:10 +0000

    Use www.liquid-build.com as canonical (matches Vercel primary domain)

--- scripts/indexnow.js
+++ scripts/indexnow.js
@@ -1,6 +1,6 @@
 // Tell Bing (and ChatGPT search, which uses Bing), Yandex and other IndexNow engines that pages changed.
 // Run after each deploy:  npm run indexnow
-const SITE = (process.env.SITE_URL || 'https://liquid-build.com').replace(/\/$/, '');
+const SITE = (process.env.SITE_URL || 'https://www.liquid-build.com').replace(/\/$/, '');
 const KEY = '31debc22d9d43e4931288c1c33d182c7';
 (async () => {
   const xml = await fetch(SITE + '/sitemap.xml').then((r) => r.text());
